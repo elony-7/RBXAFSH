3fa211fb3f3b4231a5fb388556d4d8c8ac5ac858
Elony-7 <elony7@outlook.com>
2025-09-07 11:08:05 +0800

    debugging

--- loader.lua
+++ loader.lua
@@ -1,15 +1,14 @@
-
--- this code below return nill?
 -- Preload module
 --local weatherCode = game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/PurchaseWeather.lua")
+--local PurchaseWeather = loadstring(weatherCode)()
 --local success, PurchaseWeather = pcall(function() return loadstring(weatherCode)() end)
 --if not success then warn("Failed to load PurchaseWeather:", PurchaseWeather) end
 
--- working code is below :
--- preload module
 local weatherCode = game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/PurchaseWeather.lua")
 local success, PurchaseWeather = pcall(function() return loadstring(weatherCode)() end)
-if not success then warn("Failed to load PurchaseWeather:", PurchaseWeather) end
+if not success then 
+    warn("Failed to load PurchaseWeather:", PurchaseWeather) 
+end
 
 -- Preload UI
 local UIcode = game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua")
