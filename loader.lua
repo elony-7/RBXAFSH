cebe656d68cb54a6066a7f3961c0f052870cadc0
Elony-7 <elony7@outlook.com>
2025-09-07 11:15:10 +0800

    debugging

--- loader.lua
+++ loader.lua
@@ -4,10 +4,10 @@
 --local success, PurchaseWeather = pcall(function() return loadstring(weatherCode)() end)
 --if not success then warn("Failed to load PurchaseWeather:", PurchaseWeather) end
 
-local weatherCode = game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/PurchaseWeather.lua")
-local success, PurchaseWeather = pcall(function() return loadstring(weatherCode)() end)
-if not success then 
-    warn("Failed to load PurchaseWeather:", PurchaseWeather) 
+local weather = game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/PurchaseWeather.lua")
+local success, PurchaseWeather = pcall(function() return loadstring(weather)() end)
+if not success then
+    warn("Failed to load PurchaseWeather:", PurchaseWeather)
 end
 
 -- Preload UI
