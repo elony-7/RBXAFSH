30e2ee21caf50606134a225e2d3c4741471d9275
Elony-7 <elony7@outlook.com>
2025-09-07 10:29:53 +0800

    reupdate moving the PurchaseWeather to loader

--- loader.lua
+++ loader.lua
@@ -4,6 +4,12 @@
 --local success, PurchaseWeather = pcall(function() return loadstring(weatherCode)() end)
 --if not success then warn("Failed to load PurchaseWeather:", PurchaseWeather) end
 
+local weatherCode = game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/PurchaseWeather.lua")
+local success, PurchaseWeather = pcall(function() return loadstring(weatherCode)() end)
+if not success then 
+    warn("Failed to load PurchaseWeather:", PurchaseWeather) 
+end
+
 -- Preload UI
 local UIcode = game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua")
 local UI = loadstring(UIcode)()
