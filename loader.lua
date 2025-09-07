06d1e65fd4940dd693a5fbdbe2c52a71cf3bb7f0
Elony-7 <elony7@outlook.com>
2025-09-07 11:54:31 +0800

    debugging

--- loader.lua
+++ loader.lua
@@ -3,9 +3,6 @@
 --local success, PurchaseWeather = pcall(function() return loadstring(weatherCode)() end)
 --if not success then warn("Failed to load PurchaseWeather:", PurchaseWeather) end
 
-local weatherCode = game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/PurchaseWeather.lua")
-local PurchaseWeather = loadstring(weatherCode)()
-
 -- Preload UI
 local UIcode = game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua")
 local UI = loadstring(UIcode)()
@@ -13,5 +10,6 @@ local UI = loadstring(UIcode)()
 -- Now call Main.lua
 local mainCode = game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/Main.lua")
 loadstring(mainCode)()  -- remove the internal HttpGet for PurchaseWeather
- 
--- The Main.lua should now use the preloaded PurchaseWeather and UI
+
+local weatherCode = game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/PurchaseWeather.lua")
+local PurchaseWeather = loadstring(weatherCode)()
