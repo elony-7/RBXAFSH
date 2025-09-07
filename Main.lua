a653834858f9901160090ba43af3393bb2c72a01
Elony-7 <elony7@outlook.com>
2025-09-07 10:12:46 +0800

    remove include PurchaseWeather.lua on the main.lua

--- Main.lua
+++ Main.lua
@@ -1,13 +1,13 @@
-local weather = game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/PurchaseWeather.lua")
-local success, PurchaseWeather = pcall(function() return loadstring(weather)() end)
-if not success then
-    warn("Failed to load PurchaseWeather:", PurchaseWeather)
-end
+local weatherCode = game:HttpGet("https://raw.githubusercontent.com/elony-7/RBXAFSH/main/PurchaseWeather.lua")
+local success, PurchaseWeather = pcall(function() return loadstring(weatherCode)() end)
+if not success then warn("Failed to load PurchaseWeather:", PurchaseWeather) end
 
 --========================
 -- UI Creation
 --========================
-local UI = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+-- Preload UI
+local UIcode = game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua")
+local UI = loadstring(UIcode)()
 
 local Window = UI:CreateWindow({
     Title = "IkanTerbang Hub",
