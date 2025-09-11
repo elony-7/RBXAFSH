bbf2789eb5861ccf4e7146e75d550abe51315f34
Elony-7 <elony7@outlook.com>
2025-09-12 03:59:21 +0800

    update

--- Module/AutoCastPerfect.lua
+++ Module/AutoCastPerfect.lua
@@ -59,7 +59,7 @@ local function castCycle()
     local clickX, clickY = getScreenCorner()
     local chargeGui = player:WaitForChild("PlayerGui"):WaitForChild("Charge")
     local bar = chargeGui:WaitForChild("Main"):WaitForChild("CanvasGroup"):WaitForChild("Bar")
-    local timeout = 5
+    local timeout = 6
     local startTime = tick()
 
     -- Hold mouse down at start
