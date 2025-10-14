129ad5f9320bb4e5d563f53831dba21625a5b593
elony-7 <elony7@outlook.com>
2025-10-14 15:20:33 +0800

    Update Autoreel.lua

--- Module/Autoreel.lua
+++ Module/Autoreel.lua
@@ -25,7 +25,7 @@ local function startSpam()
             pcall(function()
                 completedRE:FireServer()
             end)
-            task.wait(1) -- 40ms delay
+            task.wait(0.3) -- 40ms delay
         end
     end)
 end
