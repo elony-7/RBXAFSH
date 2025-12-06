c7b7826b96e22f91811dcc85b049ceefd7592c83
elony-7 <elony7@outlook.com>
2025-12-06 23:02:28 +0800

    Update autofishingtest.lua

--- Module/autofishingtest.lua
+++ Module/autofishingtest.lua
@@ -52,7 +52,7 @@ function AutoFishing.Start()
         while AutoFishing.Enabled do
             
             -- tiny delay to prevent self-lag but still very fast
-            task.wait(0.025)
+            task.wait(0.005)
 
             local netFolder = ReplicatedStorage
                 :FindFirstChild("Packages")
